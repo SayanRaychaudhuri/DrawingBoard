fa0b33bce2989f8f51e4df4c567fa31beedfa507
SayanRaychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-18 22:15:11 -0500

    Add some pre-made colors

---
+++ src/Colors.java
@@ -0,0 +1,14 @@
+public class Colors {
+    public static final int BLACK = 0x000000;
+    public static final int RED = 0xFF0000;
+    public static final int GREEN = 0x00FF00;
+    public static final int BLUE = 0x0000FF;
+    public static final int YELLOW = RED + GREEN;
+    public static final int CYAN = GREEN + BLUE;
+    public static final int MAGENTA = RED + BLUE;
+    public static final int WHITE = RED + GREEN + BLUE;
+    public static final int GRAY = 0x808080;
+    public static final int BROWN = 0x8B4513;
+    public static final int ORANGE = 0xFFA500;
+    public static final int LIGHTGREEN = 0x52FF52;
+}
