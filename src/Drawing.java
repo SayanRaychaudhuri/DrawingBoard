c312391e7dd6b8a6360304d0699f08e67c791016
Sayan Raychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-20 17:34:31 -0500

    Move contents of Image to Drawing

--- src/Drawing.java
+++ src/Drawing.java
@@ -4,25 +4,25 @@ public class Drawing extends DrawingBoard {
         super(width, height);
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
     public Drawing(String filePath) {
         super(filePath);
     }
 
-    public void add(Shape thing, Drawing db) {
-        if (!thing.within(db)) {
-            System.out.println("Shape is outside the drawing");
-        } else {
-            thing.drawOn(db);
-        }
+    public static void main(String[] args) {
+        Drawing space = new Drawing("C:\\Users\\Programming 1\\IdeaProjects\\DrawingBoard\\src\\EdgeOfSpace.JPG");
+        Shape tire1 = new Rectangle(400, 460, 100, 100, Colors.BLACK);
+        Shape tire2 = new Rectangle(700, 460, 100, 100, Colors.BLACK);
+        Shape rim1 = new Rectangle(400, 460, 70, 70, Colors.GRAY);
+        Shape rim2 = new Rectangle(700, 460, 70, 70, Colors.GRAY);
+        Shape carBody = new Rectangle(550, 400, 600, 120, Colors.RED);
+        Shape fin = new Triangle(250, 300, 450, 400, Colors.RED, Triangle.Orientation.UPRIGHT);
+        Shape windShieldRim = new LineSegment(700, 250, 850, 340, Colors.RED);
+        Shape windShield = new Triangle(700, 250, 850, 340, Colors.CYAN, Triangle.Orientation.UPRIGHT);
+        Shape head = new Rectangle(650, 310, 50, 50, Colors.WHITE);
+
+        Shape[] s = {carBody, tire1, tire2, rim1, rim2, fin, head, windShield, windShieldRim};
+        Group car = new Group(s);
+        car.drawOn(space);
+        space.show();
     }
-
-
 }
