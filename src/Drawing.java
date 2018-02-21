45c0c2cf7e08e7cd65934935b41267191baa8e5c
Sayan Raychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-20 20:19:25 -0500

    Implement add() and show() methods for Tester

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 public class Drawing extends DrawingBoard {
+    ArrayList<Drawable> drawables = new ArrayList<>();
 
     public Drawing(int width, int height) {
         super(width, height);
@@ -8,21 +11,14 @@ public class Drawing extends DrawingBoard {
         super(filePath);
     }
 
-    public static void main(String[] args) {
-        Drawing space = new Drawing("C:\\Users\\Programming 1\\IdeaProjects\\DrawingBoard\\src\\EdgeOfSpace.JPG");
-        Shape tire1 = new Rectangle(400, 460, 100, 100, Colors.BLACK);
-        Shape tire2 = new Rectangle(700, 460, 100, 100, Colors.BLACK);
-        Shape rim1 = new Rectangle(400, 460, 70, 70, Colors.GRAY);
-        Shape rim2 = new Rectangle(700, 460, 70, 70, Colors.GRAY);
-        Shape carBody = new Rectangle(550, 400, 600, 120, Colors.RED);
-        Shape fin = new Triangle(250, 300, 450, 400, Colors.RED, Triangle.Orientation.UPRIGHT);
-        Shape windShieldRim = new LineSegment(700, 250, 850, 340, Colors.RED);
-        Shape windShield = new Triangle(700, 250, 850, 340, Colors.CYAN, Triangle.Orientation.UPRIGHT);
-        Shape head = new Rectangle(650, 310, 50, 50, Colors.WHITE);
+    public void add(Drawable drawable){
+        drawables.add(drawable);
+    }
 
-        Shape[] s = {carBody, tire1, tire2, rim1, rim2, fin, head, windShield, windShieldRim};
-        Group car = new Group(s);
-        car.drawOn(space);
-        space.show();
+    public void show(){
+        for (Drawable aDrawable : drawables){
+            aDrawable.drawOn(this);
+        }
+        super.show();
     }
 }
